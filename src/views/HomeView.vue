767d4024338539c297badd2dc52e178dc744999b
Axel <axxal@protonmail.ch>
2022-09-04 18:38:45 +0200

    Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    data() {
+      return {
+        time: "00:00:00",
+        secretClicked: 0
+      }
+    },
+
+    mounted() {
+      this.setTime().then(() => setInterval(this.setTime, 1000));
+    },
+
+    methods: {
+      async setTime() {
+        this.time = new Date().toLocaleTimeString();
+      }
+    },
+
+    watch: {
+      secretClicked() {
+        if (this.secretClicked === 5) {
+          this.$router.push("/secret");
+        }
+      }
+    }
+  });
+</script>
+
+<template>
+  <div class="h-screen flex flex-col px-4 py-8 sm:py-4 sm:items-center justify-start sm:justify-center">
+
+    <div class="max-w-lg">
+      <h1 class="text-white text-xl sm:text-4xl font-vcr animation-vcr">Salutations.</h1>
+      <p class="text-white text-lg sm:text-lg font-vcr animation-vcr">I build software for the web.</p>
+      <p class="text-white text-lg sm:text-lg font-vcr animation-vcr">
+        The time is ticking <span @click="secretClicked++">{{ time }}</span>! Go check out my <span class="animation-vcr-glow"><a href="https://github.com/AxxAL" target="_blank">GitHub</a></span> page!
+      </p>
+    </div>
+
+  </div>
+
+  <img v-if="secretClicked >= 3" class="w-auto h-8 absolute bottom-0 left-8" src="../assets/images/dancing-banana.gif" alt="Animated pixel bananaman">
+</template>
---
+++ src/views/SecretView.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    
+});
+</script>
+
+
+<template>
+    <div class="h-screen flex flex-col justify-center items-center px-8">
+        <h1 class="text-xl sm:text-4xl font-vcr animation-vcr text-white">Congratulations.</h1>
+        <p class="text-base sm:text-2xl font-vcr animation-vcr text-white">You are one of the few subjects who have passed our test.</p>
+        <p class="text-base sm:text-2xl font-vcr animation-vcr text-white">You are now eligible to help us create a greater society.</p>
+        <p class="text-base sm:text-2xl font-vcr animation-vcr text-white">We will be in contact soon enough. In the meantime, please read up on <a class="animation-vcr-glow" href="https://r.mtdv.me/shiningmole" target="_blank">this</a>.</p>
+    </div>
+</template>
